936854517b8a6670555ac3500915050b49085545
Brian Horner <bhorner@suffolk.edu>
2022-06-14 16:44:21 -0400

    Initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+// Hello World Function
+//
+// Author: Brian Horner
+// Edit History:
+// 6/14/2022 - Initial Version
+
+#include <stdio.h> // Standard input output library
+int hello(){
+    printf("Hello, World!\n");
+    return 0;
+}
